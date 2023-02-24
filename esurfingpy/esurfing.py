3da2430e0403eccfa4a04df52954f094f85749c2
Pandaft <Allovesx@gmail.com>
2023-02-24 21:48:52 +0800

    perf: RSA encrypt `PyExecJS` replace by `rsa`

--- esurfingpy/esurfing.py
+++ esurfingpy/esurfing.py
@@ -1,8 +1,10 @@
 import re
+import rsa
 import json
 import time
+import binascii
 import requests
-from . import ocr, rsa, logs
+from . import ocr, logs
 
 DEFAULT_ESURFING_URL = "enet.10000.gd.cn:10001"
 
@@ -161,7 +163,18 @@ def login(account: str, password: str,
         try:
             log_time = time.time()
             log('正在计算 loginKey ...')
-            login_key = rsa.encrypt(account, password, ocr_result)
+            modules = int("b2867727e19e1163cc084ea57b9fa8406a910c6703413fa7df96c1acdca7b983"
+                          "a262e005af35f9485d92cd4c622eca4a14d6fd818adca5cae73d9d228b4ef05d"
+                          "732b41fb85f80af578a150ebd9a2eb5ececb853372ca4731ca1c868689298740"
+                          "9be3247f9b26cae8e787d8c135fc0652ec0678a5eda0c3d95cc1741517c0c9c3", 16)
+            public_key = rsa.PublicKey(modules, 65537)
+            data = json.dumps({
+                "userName": account,
+                "password": password,
+                "rand": ocr_result,
+            })
+            encrypted_data = rsa.encrypt(data.encode(), public_key)
+            login_key = binascii.b2a_hex(encrypted_data).decode()
             time_taken = round(time.time() - log_time, 2)
             log(f"完成计算，耗时：{time_taken}s")
         except Exception as exc:
