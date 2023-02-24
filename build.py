7b6d7ef0fe542df66e59b7ccb358b9899bdbf6a3
Pandaft <Allovesx@gmail.com>
2023-02-25 00:45:11 +0800

    ci: optimize build

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ onnx_path = os.path.join(dir_path, onnx_name)
 os.system(
     f"{python} -m nuitka "
     f"--assume-yes-for-downloads "
-    f"--include-data-files={onnx_path}=ddddocr\{onnx_name} "
+    f"--include-data-files={onnx_path}=ddddocr\\{onnx_name} "
     f"--output-dir=dist "
     f"--output-filename=ESurfingPy-CLI "
     f"--remove-output "
