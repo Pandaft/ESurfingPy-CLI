5b74fde0ff309157ca5842864711e5cf8e7bf53b
Pandaft <Allovesx@gmail.com>
2023-03-16 16:50:27 +0800

    refactor(log): more object-oriented

--- esurfingpy/log.py
+++ esurfingpy/log.py
@@ -12,28 +12,27 @@ logging.basicConfig(
 
 class Logger(logging.Logger):
 
-    def __init__(self, name: str):
-        super().__init__(name)
-        self.log = logging.getLogger(name)
+    def __new__(cls, name):
+        return logging.getLogger(name)
 
     def debug(self, msg: object, *args, **kwargs) -> object:
-        self.log.debug(msg, *args, **kwargs)
+        super().debug(msg, *args, **kwargs)
         return msg
 
     def info(self, msg: object, *args, **kwargs) -> object:
-        self.log.info(msg, *args, **kwargs)
+        super().info(msg, *args, **kwargs)
         return msg
 
     def warning(self, msg: object, *args, **kwargs) -> object:
-        self.log.warning(msg, *args, **kwargs)
+        super().warning(msg, *args, **kwargs)
         return msg
 
     def error(self, msg: object, *args, **kwargs) -> object:
-        self.log.error(msg, *args, **kwargs)
+        super().error(msg, *args, **kwargs)
         return msg
 
     def critical(self, msg: object, *args, **kwargs) -> object:
-        self.log.critical(msg, *args, **kwargs)
+        super().critical(msg, *args, **kwargs)
         return msg
 
 
