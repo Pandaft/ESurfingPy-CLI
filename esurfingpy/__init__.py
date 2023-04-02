535db3dfb3d5d19b996852c7e79fde5b9fdb1988
Pandaft <Allovesx@gmail.com>
2023-04-02 21:47:36 +0800

    chore: supply imports

--- esurfingpy/__init__.py
+++ esurfingpy/__init__.py
@@ -1,4 +1,7 @@
+from .auto import relogin
 from .cli import cli
-from .esurfing import ESurfing, login, logout
+from .esurfing import ESurfing, login, logout, get_parameters
 from .gui import Gui
 from .log import log
+from .net import Net, is_networked
+from .ocr import ocr_image
